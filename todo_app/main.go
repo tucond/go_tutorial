f6bc42f2695079a3c3ff3bf43dd75a6f57aa4b08
tucond <kikkikukkuku@gmail.com>
2022-07-07 06:46:56 +0900

    add database

--- todo_app/main.go
+++ todo_app/main.go
@@ -4,45 +4,58 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-gorm/gorm"
-	_ "github.com/mattn/go-sqlite3" //for gorm
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Todo struct {
+	gorm.Model
+	Memo string
+}
+
 func main() {
-	router:=gin.Default()
-	router.Static("styles","./styles")
+	router := gin.Default()
+	router.Static("styles", "./styles")
 	router.LoadHTMLGlob("./templates/*.html")
 	dbInit()
-	router.GET("/",getHandler)
-	router.POST("/",postHandler)
+	router.GET("/", getHandler)
+	router.POST("/new", postHandler)
 	router.Run()
 }
 
 func getHandler(ctx *gin.Context) {
 	todo := getAll()
-	ctx.HTML(200,"index.html",gin.H{"todo":todo})
+	ctx.HTML(200, "index.html", gin.H{"todo": todo})
 }
 
-func postHandler(ctx *gin.Context)
+func postHandler(ctx *gin.Context) {
 	memo := ctx.PostForm("memo")
 	create(memo)
 	ctx.Redirect(302, "/")
 }
 
-func getAll() []Todo{ 
-	db,err := gorm.Open("sqlite3","todo.sqlite3")
-	if err != nil{
-		panic("failed to connect db")
+func getAll() []Todo {
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db¥n")
 	}
 	var todo []Todo
-	dv.Find(&todo)
+	db.Find(&todo)
 	return todo
 }
 
-func dbInit(){
-
+func dbInit() {
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db¥n")
+	}
+	db.AutoMigrate(&Todo{})
 }
 
-func create(memo string){
-
+func create(memo string) {
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db¥n")
+	}
+	db.Create(&Todo{Memo: memo})
 }
