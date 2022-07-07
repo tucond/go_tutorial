c9f9411e221932962b98734b3a72d8121e27f2c1
tucond <kikkikukkuku@gmail.com>
2022-07-08 08:54:53 +0900

    add delete function

--- todo_app/main.go
+++ todo_app/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"strconv"
 )
 
 type Todo struct {
@@ -18,27 +19,44 @@ func main() {
 	router.Static("styles", "./styles")
 	router.LoadHTMLGlob("./templates/*.html")
 	dbInit()
-	router.GET("/", getHandler)
-	router.POST("/new", postHandler)
+
+	router.GET("/", listHandler)
+	router.POST("/new", createHandler)
+	router.GET("delete/:id", deleteHandler)
+
 	router.Run()
 }
 
-func getHandler(ctx *gin.Context) {
-	todo := getAll()
+func listHandler(ctx *gin.Context) {
+	todo := dbGetAll()
 	ctx.HTML(200, "index.html", gin.H{"todo": todo})
 }
 
-func postHandler(ctx *gin.Context) {
+func createHandler(ctx *gin.Context) {
 	memo := ctx.PostForm("memo")
-	create(memo)
+	dbCreate(memo)
 	ctx.Redirect(302, "/")
 }
 
-func getAll() []Todo {
+func deleteHandler(ctx *gin.Context) {
+	n := ctx.Param("id")
+	id, err := strconv.Atoi(n)
+	if err != nil {
+		panic(err)
+	}
+	dbDelete(id)
+	ctx.Redirect(302, "/")
+}
+
+func dbGetAll() []Todo {
 	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect db¥n")
 	}
+
+	sqlDb, err := db.DB() //https://github.com/go-gorm/gorm/issues/3216
+	defer sqlDb.Close()
+
 	var todo []Todo
 	db.Find(&todo)
 	return todo
@@ -49,13 +67,35 @@ func dbInit() {
 	if err != nil {
 		panic("failed to connect db¥n")
 	}
+
+	sqlDb, err := db.DB()
+	defer sqlDb.Close()
+
 	db.AutoMigrate(&Todo{})
 }
 
-func create(memo string) {
+func dbCreate(memo string) {
 	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect db¥n")
 	}
+
+	sqlDb, err := db.DB()
+	defer sqlDb.Close()
+
 	db.Create(&Todo{Memo: memo})
+}
+
+func dbDelete(id int) {
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db¥n")
+	}
+
+	sqlDb, err := db.DB()
+	defer sqlDb.Close()
+
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
 }
