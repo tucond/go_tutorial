db7bffdd875f91c89db4615070710e54b80b53d3
tucond <kikkikukkuku@gmail.com>
2022-07-10 08:19:34 +0900

    add blank api handler

--- youtube-manager/youtube-manager-go/server.go
+++ youtube-manager/youtube-manager-go/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/valyala/fasthttp"
+	"youtube-manager-go/routes"
 )
 
 func main() {
 	e := echo.New()
+
+	routes.Init(e)
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- youtube-manager/youtube-manager-go/web/api/fetch_most_popular_videos.go
+++ youtube-manager/youtube-manager-go/web/api/fetch_most_popular_videos.go
@@ -1,14 +1,12 @@
-package api 
+package api
 
 import (
 	"github.com/labstack/echo"
 	"github.com/valyala/fasthttp"
 )
 
-func FetchMostPopularVideos() {
-	echo.HandlerFunc{
-		return func(c echo.Context) error {
-			return c.JSON(fasthttp.StatusOK, "Most Popular")
-		}
+func FetchMostPopularVideos() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(fasthttp.StatusOK, "Most Popular")
 	}
 }
