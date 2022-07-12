bcb9a885afdb0e86d7da4342ac5fcf6d6412e39c
tucond <kikkikukkuku@gmail.com>
2022-07-13 08:40:25 +0900

    nuxt commit

---
+++ youtube-manager/youtube-manager-nuxt/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <div class="container">
+    </div>
+  </section>
+</template>
+
+<script>
+import ROUTES from '~/routes/api'
+export default {
+  async fetch({store}){
+    const payload={
+      uri:ROUTES.GET.POPULARS
+    }
+    await store.dispatch('fetchPopularVideos',payload)
+  }
+} 
+</script>
