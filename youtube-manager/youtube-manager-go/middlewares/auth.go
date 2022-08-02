5603d3ca7bb7379b48c0b0e4f6e05b122f4759ab
tucond <kikkikukkuku@gmail.com>
2022-08-03 08:04:58 +0900

    Fix authorization errors

--- youtube-manager/youtube-manager-go/middlewares/auth.go
+++ youtube-manager/youtube-manager-go/middlewares/auth.go
@@ -10,7 +10,7 @@ import (
 
 func verifyFirebaseIDToken(ctx echo.Context, auth *auth.Client) (*auth.Token, error) {
 	headerAuth := ctx.Request().Header.Get("Authorization")
-	token := strings.Replace(headerAuth, "Bearer", "", 1)
+	token := strings.Replace(headerAuth, "Bearer ", "", 1)
 	jwtToken, err := auth.VerifyIDToken(context.Background(), token)
 	return jwtToken, err
 }
--- youtube-manager/youtube-manager-nuxt/plugins/axios.js
+++ youtube-manager/youtube-manager-nuxt/plugins/axios.js
@@ -2,7 +2,7 @@ export default({$axios, app})=>{
     $axios.onRequest(config => {
         const token = app.$cookies.get('jwt_token');
         if(token){
-            config.headers.common['Authorizatioon'] = `Bearer ${token}`
+            config.headers.common['Authorization'] = `Bearer ${token}`
         }
     })
 }
