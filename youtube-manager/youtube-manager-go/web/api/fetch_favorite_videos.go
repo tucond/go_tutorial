d4ae11c9630e96871a3a9c9c68afb2402f515a48
tucond <kikkikukkuku@gmail.com>
2022-08-04 07:23:27 +0900

    Add favorite list function

---
+++ youtube-manager/youtube-manager-go/web/api/fetch_favorite_videos.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"firebase.google.com/go/auth"
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+	"google.golang.org/api/youtube/v3"
+	"youtube-manager-go/middlewares"
+	"youtube-manager-go/models"
+)
+
+func FetchFavoriteVideos() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		dbs := c.Get("abs").(*middlewares.DatabaseClient)
+		token := c.Get("auth").(*auth.Token)
+		user := models.User{}
+		dbs.DB.Table("users").Where(models.User{UID: token.UID}).First(&user)
+		favorites := []models.Favorite{}
+		dbs.DB.Model(&user).Association("favorites").Find(&favorites)
+		videoIds := ""
+
+		for _, f := range favorites {
+			if len(videoIds) == 0 {
+				videoIds += f.VideoId
+			} else {
+				videoIds += "," + f.VideoId
+			}
+		}
+
+		yts := c.Get("yts").(*youtube.Service)
+		call := yts.Videos.List([]string{"Id", "Snippet"}).Id(videoIds).MaxResults(10)
+		res, err := call.Do()
+		if err != nil {
+			logrus.Fatalf("Error calling Youtube API: %v", err)
+		}
+		return c.JSON(fasthttp.StatusOK, res)
+	}
+}
