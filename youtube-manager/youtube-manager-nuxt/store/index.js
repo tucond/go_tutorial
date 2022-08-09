0e26b9d2fe7f5d33fb1694b5a1cd2032b23a314a
tucond <kikkikukkuku@gmail.com>
2022-08-10 08:05:53 +0900

    Update to use refresh token

--- youtube-manager/youtube-manager-nuxt/store/index.js
+++ youtube-manager/youtube-manager-nuxt/store/index.js
@@ -14,13 +14,13 @@ export const state = () => ({
 
 export const actions = {
     async fetchPopularVideos({commit},payload){
-        const client = createRequestClient(this.$axios)
+        const client = createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.get(payload.uri, payload.params)
         commit('mutatePopularVideos',res)
     },
 
     async findVideo({commit},payload){
-        const client = createRequestClient(this.$axios)
+        const client = createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.get(payload.uri)
         const params = {
             ...res.video_list,
@@ -30,13 +30,13 @@ export const actions = {
     },
 
     async fetchRelatedVideos({commit},payload){
-        const client = createRequestClient(this.$axios)
+        const client = createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.get(payload.uri)
         commit ('mutateRelatedVideos',res)
     },
 
     async searchVideos({commit},payload){
-        const client=createRequestClient(this.$axios)
+        const client=createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.get(payload.uri, payload.params)
         commit ('mutateSearchVideos', res)
     },
@@ -46,6 +46,8 @@ export const actions = {
         const res = await firebase.auth().signInWithEmailAndPassword(payload.email,payload.password)
         const token = await res.user.getIdToken()
         this.$cookies.set('jwt_token',token)
+        const refreshToken = res.user.refreshToken
+        this.$cookies.set('refresh_token', refreshToken)
         commit('mutateToken',token)
         this.app.router.push('/')
     },
@@ -54,6 +56,8 @@ export const actions = {
         const res = await firebase.auth().signInWithEmailAndPassword(payload.email,payload.password)
         const token = await res.user.getIdToken()
         this.$cookies.set('jwt_token',token)
+        const refreshToken = res.user.refreshToken
+        this.$cookies.set('refresh_token', refreshToken)
         commit('mutateToken',token)
         this.app.router.push('/')
     },
@@ -66,17 +70,18 @@ export const actions = {
     },
 
     async setToken({commit},payload){
+        this.$cookies.set('jwt_token', payload)
         commit('mutateToken',payload)
     },
 
     async toggleFavorite({commit},payload){
-        const client = createRequestClient(this.$axios)
+        const client = createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.post(payload.uri)
         commit('mutateToggleFavorite',res.is_favorite)
     },
 
     async fetchFavoriteVideos({commit}, payload){
-        const client = createRequestClient(this.$axios)
+        const client = createRequestClient(this.$axios, this.$cookies, this)
         const res = await client.get(payload.uri)
         commit('mutateFavoriteVideos',res)
     }
